dde0b6a43979e40081d0bdc726dd0384559db459
DK <13776265+i386net@users.noreply.github.com>
2020-07-04 10:09:26 +0300

    user and card schemas

---
+++ models/card.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const cardSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectID,
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectID],
+    ref: 'user',
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
