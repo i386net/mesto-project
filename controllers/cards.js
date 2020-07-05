be48d178df9c341ccda3ddfea18c290563ea3091
DK <13776265+i386net@users.noreply.github.com>
2020-07-05 08:47:42 +0300

    add get cards

---
+++ controllers/cards.js
@@ -0,0 +1,9 @@
+const Card = require('../models/card');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = { getCards };
