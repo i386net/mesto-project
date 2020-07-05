c0b206446b038644f2eb76cc9072b91e08451055
DK <13776265+i386net@users.noreply.github.com>
2020-07-05 09:06:24 +0300

    add card posting

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,4 +6,11 @@ const getCards = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports = { getCards };
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = { getCards, createCard };
