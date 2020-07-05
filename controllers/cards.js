6e6a519a1b963a6559868ce3c5e82a511471a2f5
DK <13776265+i386net@users.noreply.github.com>
2020-07-05 13:27:55 +0300

    fix error messages

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,21 +3,21 @@ const Card = require('../models/card');
 const getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(500).send({ error: err.message }));
 };
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(500).send({ error: err.message }));
 };
 
 const deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
     .orFail(() => new Error(`Карточка с _id ${req.params.cardId} не найдена`))
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(404).send({ message: err.message }));
+    .then((card) => res.send({ data: card, message: 'Карточка удалена' }))
+    .catch((err) => res.status(404).send({ error: err.message }));
 };
 
 module.exports = { getCards, createCard, deleteCard };
