16aad72249415cdbe7835fbda62a95bad915b08a
DK <13776265+i386net@users.noreply.github.com>
2020-07-07 07:57:16 +0300

    deleteCard: add card owner verification

--- controllers/cards.js
+++ controllers/cards.js
@@ -21,9 +21,17 @@ const createCard = (req, res) => {
 
 const deleteCard = (req, res) => {
   if (mongoose.Types.ObjectId.isValid(req.params.cardId)) {
-    return Card.findByIdAndDelete(req.params.cardId)
+    return Card.findById(req.params.cardId)
       .orFail(() => new Error(`Карточка с _id ${req.params.cardId} не найдена`))
-      .then((card) => res.send({ data: card, message: 'Карточка удалена' }))
+      .then((card) => {
+        if (card.owner.toString() === req.user._id) {
+          return Card.findByIdAndDelete(card._id)
+            .orFail(() => new Error('С удалением что-то пошло не так'))
+            .then((deletedCard) => res.send({ data: deletedCard, message: 'Карточка успешно удалена' }))
+            .catch((err) => res.status(404).send({ error: err.message}));
+        }
+        return res.status(403).send({ error: 'Вы не можете удалять чужие карточки' });
+      })
       .catch((err) => res.status(404).send({ error: err.message }));
   }
   return res.status(400).send({ error: 'Неверный формат id карточки' });
