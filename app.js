8a2aa145284f8127901bcba0f0526d5e118553af
DK <13776265+i386net@users.noreply.github.com>
2020-07-08 06:27:27 +0300

    add connection error handle for mongoose

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { DB_HOST, PORT, WEB_HOST } = require('./appdata/appdata');
+const colors = require('colors');
+const {
+  dbOptions, DB_HOST, PORT, WEB_HOST,
+} = require('./appdata/appdata');
 
 const app = express();
 app.use((req, res, next) => {
   req.user = {
     _id: '5f0076239de148b63ec37d15', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
-
   next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect(DB_HOST, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(DB_HOST, dbOptions)
+  .then(() => console.log('Соединение с БД установлено:', colors.blue(DB_HOST)))
+  .catch((err) => console.log('Ошибка соединения с БД:'.red, err.message));
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
@@ -29,5 +28,4 @@ app.use('*', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Веб сервер работает по адресу: ${WEB_HOST}:${PORT}`);
-  console.log(`Сервер БД работает по адресу: ${DB_HOST}`);
 });
