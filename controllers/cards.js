c6568eadce57405ae819d4723631d2c2a6082aab
DK <13776265+i386net@users.noreply.github.com>
2020-07-08 11:50:25 +0300

    add cardId verification for like and dislike controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -37,23 +37,33 @@ const deleteCard = (req, res) => {
   return res.status(400).send({ error: 'Неверный формат id карточки' });
 };
 
-const likeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-  { new: true },
-)
-  .orFail(() => new Error(`Карточка с _id ${req.params.cardId} не найдена`))
-  .then((card) => res.send({ data: card }))
-  .catch((err) => res.status(500).send({ error: err.message }));
+const likeCard = (req, res) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.cardId)) {
+    return Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+      { new: true },
+    )
+      .orFail(() => new Error(`Карточка с _id ${req.params.cardId} не найдена`))
+      .then((card) => res.send({ data: card }))
+      .catch((err) => res.status(500).send({ error: err.message }));
+  }
+  return res.status(400).send({ error: 'Неверный формат id карточки' });
+};
 
-const dislikeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } }, // убрать _id из массива
-  { new: true },
-)
-  .orFail(() => new Error(`Карточка с _id ${req.params.cardId} не найдена`))
-  .then((card) => res.send({ data: card }))
-  .catch((err) => res.status(500).send({ error: err.message }));
+const dislikeCard = (req, res) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.cardId)) {
+    return Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true },
+    )
+      .orFail(() => new Error(`Карточка с _id ${req.params.cardId} не найдена`))
+      .then((card) => res.send({ data: card }))
+      .catch((err) => res.status(500).send({ error: err.message }));
+  }
+  return res.status(400).send({ error: 'Неверный формат id карточки' });
+};
 
 module.exports = {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
