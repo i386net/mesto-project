01cc8ec5ad8fc80436724642c69985f426de5653
DK <13776265+i386net@users.noreply.github.com>
2020-07-19 13:36:05 +0300

    add signin and signup routes, remove createUser from users route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const {
 const {
   dbOptions, DB_HOST, PORT, WEB_HOST,
 } = require('./appdata/appdata');
+const { login, createUser } = require('./controllers/users');
 
 const app = express();
 const limiter = rateLimit({
@@ -18,13 +19,16 @@ app.use((req, res, next) => {
     _id: '5f054f80fdc156787a88ac64', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
   next();
-});
+}); // todo delete
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose
   .connect(DB_HOST, dbOptions)
   .then(() => console.log('Соединение с БД установлено:', colors.blue(DB_HOST)))
   .catch((err) => console.log('Ошибка соединения с БД:'.red, err.message));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const {
-  getUsers, getUser, createUser, updateAvatar, updateUser,
+  getUsers, getUser, updateAvatar, updateUser,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
-router.post('/', createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
 
