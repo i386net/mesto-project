0d748f4f664bbd6b01310f159a8526e518181f94
DK <13776265+i386net@users.noreply.github.com>
2020-07-21 19:03:03 +0300

    update: reformat errhandling, rename cards handler

--- middlewares/errhandling.js
+++ middlewares/errhandling.js
@@ -1,13 +1,15 @@
-module.exports.errHandling = (err, req, res) => {
+const cardNotFoundErrHandling = (err, req, res) => {
   if (err.name === 'DocumentNotFoundError') {
     return res.status(404).send({ error: `Карточка с _id ${req.params.cardId} не найдена` });
   }
   return res.status(500).send({ error: err.message });
 };
 
-module.exports.validationErrorHandling = (err, res) => {
+const validationErrorHandling = (err, res) => {
   if (err.name === 'ValidationError') {
     return res.status(400).send({ error: err.message });
   }
   return res.status(500).send({ error: err.message });
 };
+
+module.exports = { cardNotFoundErrHandling, validationErrorHandling };
