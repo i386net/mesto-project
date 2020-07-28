0347cb22d3dc6c0a0ad987fb3e57b59afbcabda5
DK <13776265+i386net@users.noreply.github.com>
2020-07-28 12:48:56 +0300

    add crash test

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ mongoose
   .catch((err) => console.log('Ошибка соединения с БД:'.red, err.message));
 app.use(requestsLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', login);
 app.post('/signup', createUser);
 app.use(auth);
