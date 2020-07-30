3b81cdf5d4648f7bbc9572a42a9a0cedb4fb228d
DK <13776265+i386net@users.noreply.github.com>
2020-07-30 18:31:19 +0300

    update: add celebrate validation to users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const { celebrate } = require('celebrate');
+const { idValidation, updateUserValidation, imageUrlValidation } = require('../middlewares/validation');
 const {
   getUsers, getUser, updateAvatar, updateUser,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:userId', getUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.get('/:userId', celebrate(idValidation), getUser);
+router.patch('/me', celebrate(updateUserValidation), updateUser);
+router.patch('/me/avatar', celebrate(imageUrlValidation), updateAvatar);
 
 module.exports = router;
