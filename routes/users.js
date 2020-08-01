8b31ccd7b99ec559e64fb0445200e4e8487bdc56
DK <13776265+i386net@users.noreply.github.com>
2020-08-01 11:05:01 +0300

    update: joi messages for user

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 Joi.objectId = require('joi-objectid')(Joi);
 const { urlRegexPattern } = require('../appdata/appdata');
+const { message } = require('../appdata/messages');
 const {
   getUsers, getUser, updateAvatar, updateUser,
 } = require('../controllers/users');
@@ -9,7 +10,7 @@ const {
 router.get('/', getUsers);
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.objectId().message('Некорректный формат id'),
+    userId: Joi.objectId().message(message.incorrectFormatID),
   }),
 }), getUser);
 
@@ -20,21 +21,21 @@ router.patch('/me', celebrate({
       .min(2)
       .max(30)
       .messages({
-        'string.pattern.base': 'Имя должно быть строкой',
-        'any.required': 'Имя обязательное поле',
-        'string.min': 'В имени должно быть не менее {#limit} знаков',
-        'string.max': 'В имени должно быть не более {#limit} знаков',
-        'string.empty': 'Имя не может быть пустым',
+        'string.pattern.base': message.userNameString,
+        'any.required': message.usernameRequired,
+        'string.min': message.userNameMin,
+        'string.max': message.userNameMax,
+        'string.empty': message.userNameEmpty,
       }),
     about: Joi.string()
       .required()
       .min(2)
       .max(30)
       .messages({
-        'any.required': 'Информация обязательное поле',
-        'string.min': 'Информация должна быть не менее {#limit} знаков',
-        'string.max': 'Информация должна быть не более {#limit} знаков',
-        'string.empty': 'Поле информации не может быть пустым',
+        'any.required': message.aboutRequired,
+        'string.min': message.aboutMin,
+        'string.max': message.aboutMax,
+        'string.empty': message.aboutEmpty,
       }),
   }),
 }), updateUser);
@@ -45,9 +46,9 @@ router.patch('/me/avatar', celebrate({
       .pattern(urlRegexPattern)
       .required()
       .messages({
-        'string.pattern.base': 'Некорректная ссылка',
-        'any.required': 'Аватар обязательное поле',
-        'string.empty': 'Аватар не может быть пустым',
+        'string.pattern.base': message.incorrectURL,
+        'any.required': message.avatarRequired,
+        'string.empty': message.avatarEmpty,
       }),
   }),
 }), updateAvatar);
