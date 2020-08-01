ccceeb07ee931aae61344a3e9b967b4c26a7e1d3
DK <13776265+i386net@users.noreply.github.com>
2020-08-01 10:15:22 +0300

    add: messages dictionary

---
+++ appdata/messages.js
@@ -0,0 +1,8 @@
+const message = {
+  cardWrongParams: 'Неправильные параметры карточки!',
+  cardForbidden: 'Вы не можете удалять чужие карточки!',
+  cardNotFound: 'Карточка с этим _id не найдена',
+  cardDelete: 'Карточка успешно удалена',
+};
+
+module.exports = { message };
