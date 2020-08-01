cac4ae939ab1441dfa6c65fdbf1129bde08c3362
DK <13776265+i386net@users.noreply.github.com>
2020-08-01 10:31:09 +0300

    update: add new messages

--- appdata/messages.js
+++ appdata/messages.js
@@ -3,6 +3,11 @@ const message = {
   cardForbidden: 'Вы не можете удалять чужие карточки!',
   cardNotFound: 'Карточка с этим _id не найдена',
   cardDelete: 'Карточка успешно удалена',
+  userNotFound: 'Пользователь с этим _id не найден',
+  userLoginBusy: 'Этот логин занят',
+  userIncorrectData: 'Переданы некорректные данные',
+  userWelcomeMessage: 'Добро пожаловать <(￣︶￣)>',
+  userAuthError: 'Ошибка авторизации (×﹏×)',
 };
 
 module.exports = { message };
